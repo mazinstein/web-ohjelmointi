9bb20ed0767e87c11d1da7eb580cd85d5f85caf9
Aleksandr <mazuninalex1961@gmail.com>
2026-03-25 11:19:13 +0200

    phpup3

--- phptasks/tehtava9/header.php
+++ phptasks/tehtava9/header.php
@@ -19,7 +19,7 @@ function navLinkClass($file, $currentPage) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Starbucks Journal</title>
-  <link rel="stylesheet" href="starbucks.css">
+  <link rel="stylesheet" href="starbucks.css?v=<?php echo filemtime('starbucks.css'); ?>">
 </head>
 <body>
   <div class="page-shell">
